c39d73de4a8774b70688bd7e3e707510037b0608
Adhamhamdy <80156244+Adhamhamdy@users.noreply.github.com>
2021-04-01 21:50:24 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,61 +1,121 @@
-#include<iostream>
+#include<iostream>;
 using namespace std;
-const int MAX = 100;
-class stack {
-private:
-	int top;
-	int item[MAX];
+
+
+class kStacks
+{
+    int* arr;   
+    int* top;  
+    int* next;  
+    int n, k;
+    int free; 
 public:
-	stack():top(-1){}
-	void push(int x) {
-		if (top >= MAX - 1) {
-			cout << "Stack Is Fulled on push";
-		}
-		else {
-			top++;
-			item[top] = x;
-		}
-	}
-	bool isEmpty() {
-		if (top == -1) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	void pop() {
-		if (isEmpty()) {
-			cout << "Stack is empty on pop";
-		}
-		else {
-		    item[top];
-			top--;
-		}
-	}
-	void print() {
-		cout << "[";
-		for (int i = top;i >= 0;i--) {
-			cout << item[i] << " ";
-		}
-		cout << "]";
-		cout << endl;
-	}
-	void getMin() {
-		int smallest = item[0];
-		for (int i = 0;i < MAX;i++) {
-			item[i] = smallest;
-		}
-		cout << smallest << endl;
-	}
+    
+    kStacks(int k, int n);
+
+
+    bool isFull() { return (free == -1); }
+
+   
+    void push(int item, int sn);
+
+
+    int pop(int sn);
+
+   
+    bool isEmpty(int sn) { return (top[sn] == -1); }
 };
-int main() {
-	stack s;
-	s.push(5);
-	s.push(10);
-	s.push(15);
-	s.pop();
-	s.push(50);
-	s.print();
-	s.getMin();
+
+
+kStacks::kStacks(int k1, int n1)
+{
+   
+    k = k1, n = n1;
+    arr = new int[n];
+    top = new int[k];
+    next = new int[n];
+
+
+    for (int i = 0; i < k; i++)
+        top[i] = -1;
+
+    free = 0;
+    for (int i = 0; i < n - 1; i++)
+        next[i] = i + 1;
+    next[n - 1] = -1;  
+}
+
+
+void kStacks::push(int item, int sn)
+{
+ 
+    if (isFull())
+    {
+        cout << "\nStack Overflow\n";
+        return;
+    }
+
+    int i = free;      
+
+  
+    free = next[i];
+
+  
+    next[i] = top[sn];
+    top[sn] = i;
+
+
+    arr[i] = item;
+}
+
+
+int kStacks::pop(int sn)
+{
+
+    if (isEmpty(sn))
+    {
+        cout << "\nStack Underflow\n";
+        return INT_MAX;
+    }
+
+
+
+    int i = top[sn];
+
+    top[sn] = next[i];  
+
+    
+    next[i] = free;
+    free = i;
+
+
+    return arr[i];
+}
+
+
+int main()
+{
+   
+    int k = 3, n = 10;
+    kStacks ks(k, n);
+
+  
+    ks.push(15, 2);
+    ks.push(45, 2);
+
+   
+    ks.push(17, 1);
+    ks.push(49, 1);
+    ks.push(39, 1);
+
+ 
+    ks.push(11, 0);
+    ks.push(9, 0);
+    ks.push(7, 0);
+
+    cout << "Popped element from stack 2 is " << ks.pop(2) << endl;
+    cout << "Popped element from stack 1 is " << ks.pop(1) << endl;
+    cout << "Popped element from stack 0 is " << ks.pop(0) << endl;
+
+    return 0;
 }
